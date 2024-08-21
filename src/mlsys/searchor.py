7de9f243ab19abdef064a4382a2eca729949b190
sokinpui <sosokinpui@gmail.com>
2024-08-21 15:13:33 +0800

    add regex_search method in searchor.py

--- src/mlsys/searchor.py
+++ src/mlsys/searchor.py
@@ -1,32 +1,78 @@
 from utils.database import Database
 
-class Search:
-  def __init__(self, search_items):
+class ESSearcher:
+  def __init__(self, search_items, read_log_db, ouput_log_db):
     self.search_items = search_items
-    self.es = Database()
+    self.input_db = read_log_db
+    self.output_db = ouput_log_db
+    # TODO: support config in config.yml
+    self.searcher_index = "lookup"
 
   # get search string
-  def get_search_string(self, system_name, system_log):
+  def __get_search_string(self, system_name, system_log):
     return self.search_items[system_name][system_log]['pattern']
 
-  def search(self):
+  # maybe this search function can change outside is more fixiable for search
+  def search(self, dest_index):
     for system_name in self.search_items:
       for system_log in self.search_items[system_name]:
-        search_string = self.get_search_string(system_name, system_log)
+        search_string = self.__get_search_string(system_name, system_log)
+        respone = self.regex_search(search_string, system_name, system_log)
+        self.mark_processed(self.input_db, system_name, system_name, system_log)
+        self.output_to_destination(self.output_db, dest_index, respone)
 
-  # to check whether field is being processed
-  def is_processed(self):
-    pass
+
+
+  def regex_search(self, search_string, system_name="", log_name=""):
+    query = {
+        "query": {
+          "bool": {
+            "must": [
+              {"regexp": {"line": search_string}},
+              ]
+            }
+          }
+        }
+    # match system_name only if log_name is empty
+    if log_name == "" and system_name != "":
+      query["query"]["bool"]["must"].append({"match": {"system": system_name}})
+    elif system_name == "" and log_name != "":
+      query["query"]["bool"]["must"].append({"match": {"log": log_name}})
+    elif system_name != "" and log_name != "":
+      query["query"]["bool"]["must"].append({"match": {"system": system_name}})
+      query["query"]["bool"]["must"].append({"match": {"log": log_name}})
+
+    response = self.input_db.search(index=system_name, body=query)
+    return response['hits']
+    # TODO: check which one is more suitable
+    # return response['hits']['hits']
 
   # mark processed flag as true
-  def mark_processed(self):
+  def mark_processed(self, db, index, system_name, system_log):
+    query = {
+        "query": {
+          "bool": {
+            "must": [
+              {"match": {"system": "your_system_value"}},
+              {"match": {"log": "your_log_value"}}
+              ]
+            }
+          },
+        "script": {
+          "source": "ctx._source.processed = true"
+          }
+        }
     pass
 
+  # TODO: assume write to stdout
   # output searched results
   def output(self):
     pass
 
+  # output to destination
+  def output_to_destination(self, dest_db, dest_index, query):
+    dest_db.insert(dest_index, query)
+
 if __name__ == "__main__":
-  import rconfig
-  config = rconfig.Config("config.yml")
+  pass
 

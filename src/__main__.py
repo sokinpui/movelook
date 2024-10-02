88751fbd49ceb5c50bfa4df4a6a0078d315db04c
sokinpui <sosokinpui@gmail.com>
2024-10-02 23:07:44 +0800

    add cli interface, remove mlsys

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,11 @@
+# from file in the same directory import class
 from collector import Collector
 from scanner import Scanner
 from config import Config
 from utils.database import Database
 import sys
+import argparse
+import os
 
 
 def main():
@@ -46,7 +49,49 @@ def main():
             scanner.stop()
             collector.stop()
 
+def collect():
+    # TODO: wrapper function of Collector.run()
+    # lock for single collector instance
+    print("Collecting data...")
+
+def process(modules):
+    # TODO: wrapper function to run modules
+    # lock for each module, so that each module run single instance
+    print(f"Processing modules: {', '.join(modules)}")
+
+def cli():
+    parser = argparse.ArgumentParser(description="MoveLook system, collect log data and process them")
+
+    # read config
+    parser.add_argument('-c', '--config', type=str, help='Path to the config file', required=True)
+
+    # collect log data
+    parser.add_argument('--collect', action='store_true', help='Collect data')
+
+    # process log data
+    parser.add_argument('--process', nargs='+', help='Process specified modules')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.config):
+        print(f"Config file {args.config} does not exist")
+        sys.exit(1)
+
+    if args.collect:
+        collect()
+
+    if args.process:
+        process(args.process)
+
+    # check if no command provided
+    tmp_args = vars(args).copy()
+    tmp_args.pop('config')
+    for i in tmp_args:
+        if (tmp_args[i]):
+            return
+    print(" no command specified")
+
 
 if __name__ == "__main__":
-    main()
+    cli()
 
--- src/mlsys/t.py
+++
@@ -1,7 +0,0 @@
-#tmake a manager instance and load and run the modules
-from modules_manager import Module_Manager
-
-manager = Module_Manager('modules')
-manager.load_modules()
-manager.run_modules()
-

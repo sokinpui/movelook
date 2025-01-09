b7c6e3b729b0905aa4b66c74a1fa59ee40d9b20b
sokinpui <sosokinpui@gmail.com>
2025-01-09 09:56:11 +0800

    no timer used

--- src/main.py
+++ src/main.py
@@ -22,17 +22,20 @@ action_handler = ActionHandler(config)
 move2buffer = BufferManager(config)
 trash_cleaner = BufferManager(config)
 
+collector.process()
+extractor.regex_search()
+action_handler.check_messages()
+
 # can be configured in the config file
-collector_timer = Timer(5 * 60)  # 5 minutes
-extractor_timer = Timer(5 * 60)  # 5 minutes
-action_handler_timer = Timer(5 * 60)  # 5 minutes
-move2buffer_timer = Timer(10 * 60 * 24)  # 10 retention_days
-trash_cleaner_timer = Timer(30 * 60 * 24)  # 30 retention_days
-
-collector_timer.set_function(collector.process)
-extractor_timer.set_function(extractor.regex_search)
-action_handler_timer.set_function(action_handler.check_messages)
-move2buffer_timer.set_function(move2buffer.move_to_buffer)
-trash_cleaner_timer.set_function(trash_cleaner.cleanup_buffer)
-
-# add logic to terminate the program
+# collector_timer = Timer(5 * 60)  # 5 minutes
+# extractor_timer = Timer(5 * 60)  # 5 minutes
+# action_handler_timer = Timer(5 * 60)  # 5 minutes
+# move2buffer_timer = Timer(10 * 60 * 24)  # 10 retention_days
+# trash_cleaner_timer = Timer(30 * 60 * 24)  # 30 retention_days
+# collector_timer.set_function(collector.process)
+# extractor_timer.set_function(extractor.regex_search)
+# action_handler_timer.set_function(action_handler.check_messages)
+# move2buffer_timer.set_function(move2buffer.move_to_buffer)
+# trash_cleaner_timer.set_function(trash_cleaner.cleanup_buffer)
+#
+# # add logic to terminate the program
